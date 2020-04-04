53c10d8e4e42ac9ab067a432df70467b84d800ea
Akash Das <akashdas.ny@gmail.com>
2020-04-04 15:52:08 -0400

    made a robot

--- parser.py
+++ parser.py
@@ -75,24 +75,36 @@ def parse_file( fname, edges, polygons, csystems, screen, color ):
             add_sphere(polygons,
                        float(args[0]), float(args[1]), float(args[2]),
                        float(args[3]), step_3d)
+            matrix_mult(csystems[-1], polygons)
+            draw_polygons(polygons, screen, color)
+            polygons = []
 
         elif line == 'torus':
             #print 'TORUS\t' + str(args)
             add_torus(polygons,
                       float(args[0]), float(args[1]), float(args[2]),
                       float(args[3]), float(args[4]), step_3d)
+            matrix_mult(csystems[-1], polygons)
+            draw_polygons(polygons, screen, color)
+            polygons = []
 
         elif line == 'box':
             #print 'BOX\t' + str(args)
             add_box(polygons,
                     float(args[0]), float(args[1]), float(args[2]),
                     float(args[3]), float(args[4]), float(args[5]))
+            matrix_mult(csystems[-1], polygons)
+            draw_polygons(polygons, screen, color)
+            polygons = []
 
         elif line == 'circle':
             #print 'CIRCLE\t' + str(args)
             add_circle(edges,
                        float(args[0]), float(args[1]), float(args[2]),
                        float(args[3]), step)
+            matrix_mult(csystems[-1], edges)
+            draw_lines(edges, screen, color)
+            edges = []
 
         elif line == 'hermite' or line == 'bezier':
             #print 'curve\t' + line + ": " + str(args)
@@ -102,6 +114,9 @@ def parse_file( fname, edges, polygons, csystems, screen, color ):
                       float(args[4]), float(args[5]),
                       float(args[6]), float(args[7]),
                       step, line)
+            matrix_mult(csystems[-1], edges)
+            draw_lines(edges, screen, color)
+            edges = []
 
         elif line == 'line':
             #print 'LINE\t' + str(args)
@@ -109,16 +124,23 @@ def parse_file( fname, edges, polygons, csystems, screen, color ):
             add_edge( edges,
                       float(args[0]), float(args[1]), float(args[2]),
                       float(args[3]), float(args[4]), float(args[5]) )
+            matrix_mult(csystems[-1], edges)
+            draw_lines(edges, screen, color)
+            edges = []
 
         elif line == 'scale':
             #print 'SCALE\t' + str(args)
             t = make_scale(float(args[0]), float(args[1]), float(args[2]))
-            matrix_mult(t, transform)
+            matrix_mult(csystems[-1], t)
+            csystems.pop()
+            csystems.append(t)
 
         elif line == 'move':
             #print 'MOVE\t' + str(args)
             t = make_translate(float(args[0]), float(args[1]), float(args[2]))
-            matrix_mult(t, transform)
+            matrix_mult(csystems[-1], t)
+            csystems.pop()
+            csystems.append(t)
 
         elif line == 'rotate':
             #print 'ROTATE\t' + str(args)
@@ -130,7 +152,9 @@ def parse_file( fname, edges, polygons, csystems, screen, color ):
                 t = make_rotY(theta)
             else:
                 t = make_rotZ(theta)
-            matrix_mult(t, transform)
+            matrix_mult(csystems[-1], t)
+            csystems.pop()
+            csystems.append(t)
 
         elif line == 'ident':
             ident(transform)
@@ -143,13 +167,24 @@ def parse_file( fname, edges, polygons, csystems, screen, color ):
             edges = []
             polygons = []
 
-        elif line == 'display' or line == 'save':
-            clear_screen(screen)
-            draw_lines(edges, screen, color)
-            draw_polygons(polygons, screen, color)
+        elif line == 'display':
+            display(screen)
 
-            if line == 'display':
-                display(screen)
-            else:
-                save_extension(screen, args[0])
+        elif line == 'save':
+            save_extension(screen, args[0])
+
+        elif line == 'push':
+            length = len(csystems[-1])
+            newMat = []
+            for i in range(length):
+                new = []
+                for j in range(len(csystems[-1][i])):
+                    new.append(csystems[-1][i][j])
+                newMat.append(new)
+            csystems.append(newMat)
+            # print(csystems)
+
+        elif line == 'pop':
+            csystems.pop()
+
         c+= 1
